b01bd86851e4cdbcca692754d5b6f379cb7056e6
Eissa Purnell <eissamonet@gmail.com>
2025-01-12 11:58:55 -0600

    navbar added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
-        <div>
+        <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
           <img src={assets.logo} alt="" />
           <ul>
             <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
@@ -12,7 +12,7 @@ const Navbar = () => {
             <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
             <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
           </ul>
-          <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Signup</button>
+          <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
         </div>
     </div>
   )
