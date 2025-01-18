37b9653d6afd9cd54d6a1b1e4aef94ecd8df9db9
Eissa Purnell <eissamonet@gmail.com>
2025-01-18 16:31:20 -0600

    added mobile screen styling

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
+
 
 
 const Navbar = () => {
    const[showMenu, setShowMenu] = useState(false)
+
+
+ useEffect (() => {
+    if(showMenu){
+      document.body.style.overflow = 'hidden'
+     }else{
+       document.body.style.overflow = 'auto'
+     }
+     return () => {
+         document.body.style.overflow = 'auto'
+     };
+  }, [showMenu])
+
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -15,12 +29,12 @@ const Navbar = () => {
             <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
           </ul>
           <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
-          <img src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt="" />
+          <img onClick={()=> setShowMenu(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt="" />
         </div>
         {/*------------------mobile menu---------- */}
         <div className={`md:hidden ${showMenu ? 'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
           <div className='flex justify-end p-6 cursor-pointer'>
-            <img src={assets.cross_icon} className='w-6' alt="" />
+            <img onClick={()=> setShowMenu(false)} src={assets.cross_icon} className='w-6' alt="" />
           </div>
           <ul className='flex flex-col items-center gap-2 mt-5 mx-5 text-lg font-medium'>
             <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
