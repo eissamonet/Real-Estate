882d672c2122abe612571f324aaab428a31abdbd
Eissa Purnell <eissamonet@gmail.com>
2025-01-18 14:21:46 -0600

    background img added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from './component/Navbar'
+import Header from './component/Header'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Header/>
     </div>
   )
 }
