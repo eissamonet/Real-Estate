918ffeff395b82c041ee7741f6cd1451ece95d1e
Eissa Purnell <eissamonet@gmail.com>
2025-01-19 15:19:07 -0600

    added About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './component/Header'
+import About from './component/About'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Header/>
+      <About/>
     </div>
   )
 }
