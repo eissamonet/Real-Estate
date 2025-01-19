a0e181f7f7c545d8e82523f52028a2f68a209a42
Eissa Purnell <eissamonet@gmail.com>
2025-01-19 15:58:43 -0600

    projects section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './component/Header'
 import About from './component/About'
+import Projects from './component/Projects'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Header/>
       <About/>
+      <Projects/>
     </div>
   )
 }
