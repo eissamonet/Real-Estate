de64f9002112ff9955affc809f473d46b2b79de3
Eissa Purnell <eissamonet@gmail.com>
2025-01-19 15:50:00 -0600

    commit changes

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -9,6 +9,30 @@ const About = () => {
        <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to Your Vision</p>
        <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
          <img src={assets.brand_img} alt="" className='w-full sm:w1/2 max-w-lg'/>
+         <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+            <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>10+</p>
+                    <p>Years of Excellence</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>12+</p>
+                    <p>Projects Completed</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>20+</p>
+                    <p>Mn. Sq. Ft. Delivered</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>25+</p>
+                    <p>Ongoing Pojects</p>
+                </div>
+            </div>
+            <p className='my-10 max-w-lg'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <button className='bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
+         </div>
        </div>
     </div>
   )
