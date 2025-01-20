4c47eb980b6e1169f463c4017fa6ada0ece704b7
Eissa Purnell <eissamonet@gmail.com>
2025-01-20 13:35:24 -0600

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './component/Header'
 import About from './component/About'
 import Projects from './component/Projects'
 import Testimonials from './component/Testimonials'
+import Contact from './component/Contact'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <About/>
       <Projects/>
       <Testimonials/>
+      <Contact/>
     </div>
   )
 }
