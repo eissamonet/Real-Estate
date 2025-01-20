606c221b6329f292f14640a094adc60c90d65afd
Eissa Purnell <eissamonet@gmail.com>
2025-01-20 17:53:07 -0600

    added toastify alerts

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import About from './component/About'
 import Projects from './component/Projects'
 import Testimonials from './component/Testimonials'
 import Contact from './component/Contact'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
